2ccf4f685d1b51a1163fc064ef77326df28ae892
Иван Селюк <ivan.seliuk12@gmail.ru>
2022-08-24 17:14:21 +0300

    upd

--- AutoLayoutTest/AutoLayoutTest/ViewController.swift
+++ AutoLayoutTest/AutoLayoutTest/ViewController.swift
@@ -18,9 +18,9 @@ class ViewController: UIViewController {
     
     let descriptionTextView: UITextView = {
         let textView = UITextView()
-        textView.text = "Kobe Bryant (1978-2020) was one of the most accomplished and celebrated athletes of all time. Over the course of his twenty-year career―all played with the Los Angeles Lakers―he won five NBA championships, two Olympic gold medals, eighteen All-Star selections, and four All-Star Game MVP awards, among many other achievements before retiring in 2016. He is Legend!"
-        textView.textColor = .white
-        textView.font = UIFont.systemFont(ofSize: 18)
+        let attributedText = NSMutableAttributedString(string: "Kobe Bryant (1978-2020) was one of the most accomplished and celebrated athletes of all time. Over the course of his twenty-year career―all played with the Los Angeles Lakers―he won five NBA championships, two Olympic gold medals, eighteen All-Star selections, and four All-Star Game MVP awards, among many other achievements before retiring in 2016.", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 18), NSAttributedString.Key.foregroundColor: UIColor.white])
+        attributedText.append(NSAttributedString(string: "\n\nHe is Legend!\n R.I.P.", attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 24), NSAttributedString.Key.foregroundColor: UIColor.yellow]))
+        textView.attributedText = attributedText
         textView.textAlignment = .center
         textView.translatesAutoresizingMaskIntoConstraints = false
         textView.isEditable = false
@@ -31,7 +31,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.addSubview(kobeBrayantImage)
         view.addSubview(descriptionTextView)
         view.backgroundColor = .black
         
@@ -40,16 +39,32 @@ class ViewController: UIViewController {
     }
     
     private func setupLayout() {
+        let topImageContainerView = UIView()
+        topImageContainerView.backgroundColor = .black
+        view.addSubview(topImageContainerView)
+        topImageContainerView.addSubview(kobeBrayantImage)
+        topImageContainerView.translatesAutoresizingMaskIntoConstraints = false
+        
+        
         view.addConstraints([
-            kobeBrayantImage.topAnchor.constraint(equalTo: view.topAnchor, constant: 0),
-            kobeBrayantImage.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
-            kobeBrayantImage.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
-            kobeBrayantImage.heightAnchor.constraint(equalToConstant: UIScreen.main.bounds.height / 2),
-            descriptionTextView.topAnchor.constraint(equalTo: kobeBrayantImage.bottomAnchor, constant: 0),
-            descriptionTextView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
-            descriptionTextView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
-            descriptionTextView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0)
+            kobeBrayantImage.topAnchor.constraint(equalTo: topImageContainerView.topAnchor, constant: 0),
+            kobeBrayantImage.trailingAnchor.constraint(equalTo: topImageContainerView.trailingAnchor, constant: 0),
+            kobeBrayantImage.leadingAnchor.constraint(equalTo: topImageContainerView.leadingAnchor, constant: 0),
+            kobeBrayantImage.bottomAnchor.constraint(equalTo: topImageContainerView.bottomAnchor, constant: 0),
+            
+            descriptionTextView.topAnchor.constraint(equalTo: topImageContainerView.bottomAnchor, constant: 0),
+            descriptionTextView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -24),
+            descriptionTextView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 24),
+            descriptionTextView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0),
+            
+            topImageContainerView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0),
+            topImageContainerView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+            topImageContainerView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            topImageContainerView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.5)
+            
         ])
+        
+    
     }
         
     
