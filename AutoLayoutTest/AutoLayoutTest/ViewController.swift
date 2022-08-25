ca516a2869454fdeae2bb777151b19e5a03d87b5
Иван Селюк <ivan.seliuk12@gmail.ru>
2022-08-25 12:38:23 +0300

    upd

--- AutoLayoutTest/AutoLayoutTest/ViewController.swift
+++ AutoLayoutTest/AutoLayoutTest/ViewController.swift
@@ -7,8 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+extension UIColor {
+   static var mainYellow = UIColor(red: 1, green: 251/255, blue: 0, alpha: 1)
+}
 
+class ViewController: UIViewController {
+    
     let kobeBrayantImage: UIImageView = {
         let imageView = UIImageView(image: UIImage(named: "Kobe Bryant"))
         imageView.translatesAutoresizingMaskIntoConstraints = false
@@ -18,8 +22,8 @@ class ViewController: UIViewController {
     
     let descriptionTextView: UITextView = {
         let textView = UITextView()
-        let attributedText = NSMutableAttributedString(string: "Kobe Bryant (1978-2020) was one of the most accomplished and celebrated athletes of all time. Over the course of his twenty-year career―all played with the Los Angeles Lakers―he won five NBA championships, two Olympic gold medals, eighteen All-Star selections, and four All-Star Game MVP awards, among many other achievements before retiring in 2016.", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 18), NSAttributedString.Key.foregroundColor: UIColor.white])
-        attributedText.append(NSAttributedString(string: "\n\nHe is Legend!\n R.I.P.", attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 24), NSAttributedString.Key.foregroundColor: UIColor.yellow]))
+        let attributedText = NSMutableAttributedString(string: "Kobe Bryant (1978-2020) was one of the most accomplished and celebrated athletes of all time. Over the course of his twenty-year career―all played with the Los Angeles Lakers―he won five NBA championships, two Olympic gold medals, eighteen All-Star selections, and four All-Star Game MVP awards, among many other achievements before retiring in 2016.", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 18), NSAttributedString.Key.foregroundColor: UIColor.lightGray])
+        attributedText.append(NSAttributedString(string: "\n\nHe is Legend!\n R.I.P.", attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 24), NSAttributedString.Key.foregroundColor: UIColor.white]))
         textView.attributedText = attributedText
         textView.textAlignment = .center
         textView.translatesAutoresizingMaskIntoConstraints = false
@@ -28,14 +32,58 @@ class ViewController: UIViewController {
         return textView
     }()
     
+    
+    private let previousButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("PREV", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.setTitleColor(UIColor.mainYellow, for: .normal)
+        return button
+    }()
+    
+    private let nextButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("NEXT", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.setTitleColor(UIColor.mainYellow, for: .normal)
+        return button
+    }()
+    
+    private let pageControl: UIPageControl = {
+        let pc = UIPageControl()
+        pc.currentPage = 0
+        pc.numberOfPages = 3
+        pc.pageIndicatorTintColor = .mainYellow
+        return pc
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.addSubview(descriptionTextView)
         view.backgroundColor = .black
         
+        setupButtonControls()
         setupLayout()
         
+    }
+    
+    fileprivate func setupButtonControls() {
+    
+        let bottomControlsStackView = UIStackView(arrangedSubviews: [previousButton, pageControl, nextButton])
+        bottomControlsStackView.translatesAutoresizingMaskIntoConstraints = false
+        bottomControlsStackView.distribution = .fillEqually
+        // bottomControlsStackView.axis = .vertical
+        view.addSubview(bottomControlsStackView)
+        
+        NSLayoutConstraint.activate([
+            bottomControlsStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            bottomControlsStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            bottomControlsStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            bottomControlsStackView.heightAnchor.constraint(equalToConstant: 50)
+        ])
     }
     
     private func setupLayout() {
@@ -45,7 +93,6 @@ class ViewController: UIViewController {
         topImageContainerView.addSubview(kobeBrayantImage)
         topImageContainerView.translatesAutoresizingMaskIntoConstraints = false
         
-        
         view.addConstraints([
             kobeBrayantImage.topAnchor.constraint(equalTo: topImageContainerView.topAnchor, constant: 0),
             kobeBrayantImage.trailingAnchor.constraint(equalTo: topImageContainerView.trailingAnchor, constant: 0),
@@ -53,23 +100,16 @@ class ViewController: UIViewController {
             kobeBrayantImage.bottomAnchor.constraint(equalTo: topImageContainerView.bottomAnchor, constant: 0),
             
             descriptionTextView.topAnchor.constraint(equalTo: topImageContainerView.bottomAnchor, constant: 0),
-            descriptionTextView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -24),
-            descriptionTextView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 24),
-            descriptionTextView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0),
+            descriptionTextView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: 0),
+            descriptionTextView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 0),
+            descriptionTextView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50),
             
             topImageContainerView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0),
             topImageContainerView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
             topImageContainerView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
             topImageContainerView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.5)
-            
+    
         ])
-        
-    
     }
-        
-    
-
-
 }
 
- 
